1359adb9b7f59504b63126c8ec958e526d7259c7
Max <cholushkin@gmail.com>
2024-12-23 00:54:09 +0200

    add(art): character

--- ExportFbxScripts/usr_export_character.py
+++ ExportFbxScripts/usr_export_character.py
@@ -2,9 +2,9 @@ import os
 from run_blender import run_blender  # Import the run_blender function
 
 # Specify the list of object names to export
-objects_to_export = ["ChunkA"]  # Modify this list as per your needs
-blend_file = "../ArtSources/lev-chunk-pack-0.blend"  # Path to your .blend file
-export_directory = "../Unity/TargetOne/Assets/Core/!Fbx/LevChunks"  # Directory to save the FBX files
+objects_to_export = ["Character"]  # Modify this list as per your needs
+blend_file = "../ArtSources/character.blend"  # Path to your .blend file
+export_directory = "../Unity/TargetOne/Assets/Core/!Fbx"  # Directory to save the FBX files
 blender_executable = "C:\\Program Files\\Blender Foundation\\Blender 4.2\\blender.exe"  # Update this path if necessary
 
 # Call the run_blender function with the specified parameters
